a4e0dd762de71ef52b0e85a04cd1ce533bf6d36d
Hans He <hjh2bs@virginia.edu>
2015-09-14 00:45:39 -0400

    Started dabbling in javascript

---
+++ js/control.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+
+	$("#experience").click(function (){
+		$("#p5").animate({
+			"left": "+=100px"
+
+		}, "fast" );
+
+	});
+
+	/*$("#experience").click(function (){
+		$("#p5").toggle("slide");
+
+	});*/
+
+});
